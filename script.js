32b6bd411d3365f449bda51b11a626d518ef6907
Millana <chotubaevam@gmail.com>
2022-01-17 09:24:29 +0600

    the fortune teller Millana

---
+++ script.js
@@ -0,0 +1,4 @@
+function tellFortune (numOfChildren, partnersName, geographicLoc, jobTitle) {
+    return `Youn will be a ` + jobTitle + ` in ` + geographicLoc + ` and married to ` + partnersName + ` with ` + numOfChildren + ` kids.`
+  }
+tellFortune(2, `Bekzhan`, `New York`, `web-developer`)
